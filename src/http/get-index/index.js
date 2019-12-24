d3e7ba48a2d84a7d65eae188397895f274915e01
unknown <samuel.breed@gmail.com>
2019-12-23 23:56:38 -0800

    csp

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -7,8 +7,9 @@ exports.handler = async function Index () {
   return {
     headers: {
       'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+      'content-security-policy': `default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self' 'sha256-Wd5QTAWValERuPK74eX4gF4bxsRa37Q35ccDTxeOCYk=';`
     },
     body
-  }
+}
 }
