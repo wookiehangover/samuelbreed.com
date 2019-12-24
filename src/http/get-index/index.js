1116f78ae25b49ecee60bacaedf6d5948bfaf98d
unknown <samuel.breed@gmail.com>
2019-12-24 00:00:37 -0800

    fix csp

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -8,7 +8,7 @@ exports.handler = async function Index () {
     headers: {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-security-policy': `default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self' 'sha256-Wd5QTAWValERuPK74eX4gF4bxsRa37Q35ccDTxeOCYk=';`
+      'content-security-policy': `default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self' '${process.env.BEGIN_STATIC_EDGE}'; style-src 'self' 'sha256-Wd5QTAWValERuPK74eX4gF4bxsRa37Q35ccDTxeOCYk=';`
     },
     body
 }
