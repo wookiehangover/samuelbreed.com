86651381a6c66ddeb07fc1ca6e37928e52ba5da9
unknown <samuel.breed@gmail.com>
2019-12-24 00:13:35 -0800

    yet another csp fix

--- src/views/csp.js
+++ src/views/csp.js
@@ -1,10 +1,11 @@
-const staticEdge = process.env.BEGIN_STATIC_EDGE || ''
+const staticEdge = process.env.NODE_ENV === 'production'
+  ? ` ${process.env.BEGIN_STATIC_EDGE}/` : ''
 
 module.exports = [
   `default-src 'none'`,
   `script-src 'self'`,
   `connect-src 'self'`,
-  `img-src 'self' ${staticEdge}`,
+  `img-src 'self'${staticEdge}`,
   `style-src 'self' 'sha256-Wd5QTAWValERuPK74eX4gF4bxsRa37Q35ccDTxeOCYk='`,
-  `manifest-src 'self' ${staticEdge}`
+  `manifest-src 'self'${staticEdge}`
 ].join('; ')
